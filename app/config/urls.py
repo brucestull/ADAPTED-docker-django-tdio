a7a38f1e79d9de8d061cd1cbff9f2ceeed7763cc
FlynntKnapp <bruce.stull@gmail.com>
2023-10-08 13:48:51 -0400

    Add urls for 'accounts' and 'admindocs'.

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,25 +1,20 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
 
+from upload.views import image_upload
+
 urlpatterns = [
-    path("", RedirectView.as_view(url="/admin/"), name="home"),
-    path("admin/doc/", include("django.contrib.admindocs.urls")),
+    path("", image_upload, name="upload"),
+    path("home/", RedirectView.as_view(pattern_name="upload"), name="home"),
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
+
+if bool(settings.DEBUG):
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
